0f44101e2ebdca427bd16c6e425b8621f6afb8f0
Oluwafemi <platvipez@gmail.com>
2023-02-02 20:11:42 -0800

    Fixed Wait-list

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import banner from '../assets/images/mybanner.png'
 
 const Hero = () => {
+const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [submitting, setSubmitting] = useState(false);
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    try {
+      const response = await fetch("https://us3.api.mailchimp.com/3.0/lists/5968090d71/members", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "apikey 29ea16558655fe049d0cdc1b38279f60-us3"
+        },
+        body: JSON.stringify({
+          email_address: email,
+          status: "pending",
+          merge_fields: {
+            PHONE: phone
+          }
+        })
+      });
+      const data = await response.json();
+      if (data.status === 400) {
+        throw new Error(data.detail);
+      }
+      setSuccess(true);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
   return (
     <div>
         <div className="flex_container">
@@ -15,14 +52,19 @@ const Hero = () => {
                     Download the MyRoute mobile app today!
                 </p>
                 <div className="form_container">
-                    <form action="">
+                    <form action="" onSubmit={handleSubmit}>
+                        {success && <p>Thank you for joining the waitlist!</p>}
+                        {error && <p>Error: {error}</p>}
                         <div className="form_control">
-                            <input type="email" name="email" id="email" placeholder='Enter Your E-mail' />
+                            <input type="email" value={email} onChange={e => setEmail(e.target.value)}
+                            disabled={submitting} name="MERGE0" id="MERGE0" placeholder='Enter Your E-mail' />
                         </div>
                         <div className="form_control">
-                            <input type="tel" name="tel" id="tel" placeholder='Your Phone Number' />
+                            <input type="tel" value={phone} 
+                            onChange={e => setPhone(e.target.value)}
+                            disabled={submitting} name="MERGE4" id="MERGE4" placeholder='Your Phone Number' />
                         </div>
-                        <button>
+                        <button type='submit' disabled={submitting}>
                             Join Our Wait-list
                         </button>
                     </form>
