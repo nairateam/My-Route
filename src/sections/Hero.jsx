0deb995e262e35eaa7b15a9e57be362f42cde225
devfemii <onabulefemi@gmail.com>
2023-10-20 12:07:22 -0700

    modified waitlist

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,112 +1,133 @@
-import React, { useState } from 'react'
-import banner from '../assets/images/mybanner.png'
+import React, { useState } from 'react';
+import axios from 'axios';
+import banner from '../assets/images/mybanner.png';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+const Form = ({ onFormSubmit, setError }) => {
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+
+  const isEmailValid = (email) => {
+    // Implement email validation logic here
+    return /\S+@\S+\.\S+/.test(email);
+  };
+
+  const isPhoneValid = (phone) => {
+    // Implement phone validation logic here
+    return /^\d{11}$/.test(phone);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!isEmailValid(email)) {
+      setError('Invalid email address');
+      toast.warn('Invalid email address');
+      return;
+    }
+
+    if (!isPhoneValid(phone)) {
+      setError('Invalid phone number');
+      toast.warn('Invalid phone number');
+      return;
+    }
+
+    onFormSubmit(email, phone);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form_control">
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          name="email"
+          id="email"
+          placeholder="Enter Your E-mail"
+        />
+      </div>
+      <div className="form_control">
+        <input
+          type="tel"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          name="phone"
+          id="phone"
+          placeholder="Your Phone Number"
+        />
+      </div>
+      <button type="submit">Join Our Wait-list</button>
+    </form>
+  );
+};
 
 const Hero = () => {
-    const [email, setEmail] = useState('');
-    const [phone, setPhone] = useState('');
-    const [message, setMessage] = useState('');
+  const [message, setMessage] = useState('');
+  const apiURL = process.env.REACT_APP_API_URL;
 
-    const postData = async (email, phone, message) => {
-        await fetch('https://myroute-eac81-default-rtdb.firebaseio.com/myroute.json', {
-            method: 'POST',
-            body: JSON.stringify({
-                email: email,
-                phone: phone,
-
-            }),
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-             },
-        })
-        .then((response) => response.json())
-        .then(() => {
-         setEmail('');
-         setPhone('');
-         setMessage(toast.success('Thank You For Joining Our Wait-list!', {
-            position: "top-center",
-            autoClose: 2000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "light",
-            }));
-      })
-      .catch((err) => {
-         console.log(err.message);
-         setMessage(err.message);   
+  const postData = async (email, phone) => {
+    try {
+      const response = await axios.post(apiURL, {
+        email: email,
+        phone: phone,
       });
-    };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        postData(email, phone, message);
-        console.log(email, phone, message)
-     };
-  
-    // const handleSubmit = (e) => {
-    //     e.preventDefault();
-    //     console.log({
-    //       email,
-    //       phone,
-    //     });
-    //     setEmail("");
-    //     setPhone("");
-    //   };
+      if (response.data.status === 200) {
+        toast.success('Thank you for joining our waitlist!');
+        console.log('Very Good!')
+      } else {
+        const data = response.data;
+        setMessage(data.message);
+      }
+    } catch (error) {
+      console.error(error);
+      setMessage('An error occurred while submitting the form.');
+      toast('An error occurred while submitting the form.');
+    }
+  };
 
+  const handleFormSubmit = (email, phone) => {
+    postData(email, phone);
+  };
+  console.log(message)
 
   return (
     <div>
-        <div className="flex_container">
-            <div className="flex_item">
-                <h2>
-                    Going somewhere? Follow <span>MyRoute!</span>
-                </h2>
-                <p>
-                    Efficient, Economical and Eco-Friendly: Carpool with us and save time, money, 
-                    and the environment amidst other exciting services we have for you. 
-                    Download the MyRoute mobile app today!
-                </p>
-                <div className="form_container">
-                    <form action="" onSubmit={handleSubmit}>
-                        <div className="form_control">
-                            <input type="email" value={email} onChange={e => setEmail(e.target.value)}
-                            name="MERGE0" id="MERGE0" placeholder='Enter Your E-mail' />
-                        </div>
-                        <div className="form_control">
-                            <input type="tel" value={phone} 
-                            onChange={e => setPhone(e.target.value)}
-                            name="MERGE4" id="MERGE4" placeholder='Your Phone Number' />
-                        </div>
-                        <button type='submit'>
-                            Join Our Wait-list
-                        </button>
-                        <ToastContainer
-                            position="top-right"
-                            autoClose={2000}
-                            hideProgressBar={false}
-                            newestOnTop={false}
-                            closeOnClick
-                            rtl={false}
-                            pauseOnFocusLoss
-                            draggable
-                            pauseOnHover
-                            theme="light"
-                            />
-                        {/* {message && <p>{message}</p>} */}
-                    </form>
-                </div>
-            </div>
-            <div className="flex_item">
-                <img src={banner} alt="Hero Banner" />
-            </div>
+      <div className="flex_container">
+        <div className="flex_item">
+          <h2>
+            Going somewhere? Follow <span>MyRoute!</span>
+          </h2>
+          <p>
+            Efficient, Economical and Eco-Friendly: Carpool with us and save time,
+            money, and the environment amidst other exciting services we have for you.
+            Download the MyRoute mobile app today!
+          </p>
+          <div className="form_container">
+            <Form onFormSubmit={handleFormSubmit} setError={setMessage} />
+            <ToastContainer
+              position="top-right"
+              autoClose={2000}
+              hideProgressBar={true}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="light"
+            />
+            {/* {message === '' ? <>ni</> : <p>{message}</p>} */}
+          </div>
         </div>
+        <div className="flex_item">
+          <img src={banner} alt="Hero Banner" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
